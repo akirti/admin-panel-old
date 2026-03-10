cab1b49b5024ec40d3fbbaa9abe90943cce864f2
akirti <akirti.iitk@gmail.com>
2026-03-10 03:47:41 -0400

    coverage issue

--- backend/src/main.py
+++ backend/src/main.py
@@ -195,6 +195,6 @@ def bootstrap():
 
 app = bootstrap()
 
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
